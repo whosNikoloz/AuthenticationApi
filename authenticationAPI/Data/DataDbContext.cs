763b74b0c30f535c4256c831c9aeca3d54c3a496
whosNikoloz <nika.kobaidze04@gmail.com>
2023-07-05 07:32:39 +0400

    init

---
+++ authenticationAPI/Data/DataDbContext.cs
@@ -0,0 +1,16 @@
+﻿using authenticationAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Options;
+
+namespace authenticationAPI.Data
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ authenticationAPI/Migrations/20230702150133_init2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace authenticationAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Verification",
+                table: "Users",
+                newName: "VerificationToken");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VerificationToken",
+                table: "Users",
+                newName: "Verification");
+        }
+    }
+}
